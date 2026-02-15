183e7c69615d0f20eb01e6db329176f4d6bd6a9e
Rahma Maalej <rahma.maalej.13@gmail.com>
2026-02-16 00:23:34 +0100

    setup app component with project management, the root was point at the default page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { ProjectListComponent } from './features/projects/components/project-list/project-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ProjectListComponent], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('AngularProject_1');
+  title = 'project-manager';
 }
