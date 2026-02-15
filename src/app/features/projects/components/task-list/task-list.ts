91e610ac9b96e24d4e3e6759f21b57906a574f59
Rahma Maalej <rahma.maalej.13@gmail.com>
2026-02-16 00:41:39 +0100

    removing getStatusColor and getPriorityBadge functions, replaced with ngClass in the html file

--- src/app/features/projects/components/task-list/task-list.ts
+++ src/app/features/projects/components/task-list/task-list.ts
@@ -17,21 +17,5 @@ interface Task {
 export class TaskListComponent {
   @Input() tasks: Task[] = [];
 
-  getStatusColor(status: string): string {
-    switch(status) {
-      case 'pending': return 'border-yellow-500 bg-yellow-50';
-      case 'In Progress': return 'border-blue-500 bg-blue-50';
-      case 'completed': return 'border-green-500 bg-green-50';
-      default: return 'border-gray-500 bg-gray-50';
-    }
-  }
-
-  getPriorityBadge(priority: string): string {
-    switch(priority) {
-      case 'High': return 'bg-red-100 text-red-800';
-      case 'Medium': return 'bg-orange-100 text-orange-800';
-      case 'Low': return 'bg-green-100 text-green-800';
-      default: return 'bg-gray-100 text-gray-800';
-    }
-  }
+  
 }
