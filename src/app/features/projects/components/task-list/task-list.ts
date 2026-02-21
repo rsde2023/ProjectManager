e59088eaf725bc3c46c857ce9135f1a558023d5b
Rahma Maalej <rahma.maalej.13@gmail.com>
2026-02-21 09:42:12 +0100

    define colors logic

--- src/app/features/projects/components/task-list/task-list.ts
+++ src/app/features/projects/components/task-list/task-list.ts
@@ -16,6 +16,15 @@ interface Task {
 })
 export class TaskListComponent {
   @Input() tasks: Task[] = [];
-
+  priorityClasses: Record<string, string> = {
+    'High': 'bg-red-100 text-red-800',
+    'Medium': 'bg-orange-100 text-orange-800',
+    'Low': 'bg-green-100 text-green-800'
+  };
+ statusClasses: Record<string, string> = {
+    pending: 'bg-yellow-100 text-yellow-800',
+    'In Progress': 'bg-blue-100 text-blue-800',
+    completed: 'bg-green-100 text-green-800'
+  };
   
 }
