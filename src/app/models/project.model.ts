97bd825bb0208bc4d912e7da8ec93b576a6bcfb4
Rahma Maalej <rahma.maalej.13@gmail.com>
2026-04-10 19:28:52 +0100

    feat: add id attribute to project interface to save projects in localstorage

--- src/app/models/project.model.ts
+++ src/app/models/project.model.ts
@@ -1,24 +1,25 @@
 type ProjectStatus = 'pending' | 'In Progress' | 'completed';
 
-interface Project {
+export interface Project {
+  id: number;
   name: string;
   description: string;
   status: string;
   tasks: Task[];
 }
-interface Task {
+export interface Task {
   title: string;
   priority: string;
   status: ProjectStatus;
 }
- interface StatusStyle {
+export interface StatusStyle {
   bgColor?: string;      // For header bar (projects)
   badgeBg: string;       // For status badge background
   badgeText: string;     // For status badge text
   dotClass?: string;     // For task dot indicator
   label: string;         // Display label
 }
-interface PriorityStyle {
+export interface PriorityStyle {
   bgClass: string;
   textClass: string;
 }
