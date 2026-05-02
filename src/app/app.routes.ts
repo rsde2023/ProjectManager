90bafd1a647d4d019c9d319ed25f4019d20f0577
Rahma Maalej <rahma.maalej.13@gmail.com>
2026-05-02 20:24:47 +0100

    feat(routing): mise en place du routing avec sidebar layout
    - Configuration des routes (projects par défaut, contact)
    - Simplification du AppComponent avec RouterOutlet
    - Mise en place du LayoutComponent avec sidebar responsive
    - Ajout du toggle pour réduire/agrandir la sidebar
    - Intégration du provideRouter dans main.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { ContactFormComponent } from './features/projects/components/contact-form/contact-form';
+import { ProjectListComponent } from './features/projects/components/project-list/project-list';
+import { LayoutComponent } from './features/projects/components/layout/layout';
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: 'projects', pathMatch: 'full' },
+      { path: 'projects', component: ProjectListComponent },
+      { path: 'contact', component: ContactFormComponent }
+    ]
+  },
+  {path: '**', redirectTo: 'projects'  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,67 +1,13 @@
-import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
-import { ProjectListComponent } from './features/projects/components/project-list/project-list';
-import { AddProjectComponent } from './features/projects/components/add-project/add-project';
-import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { Project } from './models/project.model';
-import { ContactFormComponent } from './features/projects/components/contact-form/contact-form';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
 @Component({
   selector: 'app-root',
-  imports: [ProjectListComponent, AddProjectComponent, ContactFormComponent, CommonModule],
+  standalone: true,
+  imports: [RouterOutlet], 
   templateUrl: './app.html',
-  styleUrl: './app.css',
+  styleUrls: ['./app.css']
 })
-export class App implements OnInit {
-  showModal = false;
-  projects: Project[] = [];
+export class App {
   title = 'project-manager';
-
-  // Injection du platformId
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
-
-  // Ouvrir le modal
-  openAddProjectModal() {
-    this.showModal = true;
-  }
-
-  // Fermer le modal
-  closeModal() {
-    this.showModal = false;
-  }
-
-  ngOnInit() {
-    // Protection SSR ici
-    if (isPlatformBrowser(this.platformId)) {
-      const savedProjects = localStorage.getItem('projects');
-      if (savedProjects) {
-        this.projects = JSON.parse(savedProjects);
-      }
-    }
-  }
-
-  onProjectAdded(project: Project) {
-    this.projects.push(project);
-    this.saveProjectsToLocalStorage();
-    this.closeModal();
-  }
-
-  onProjectDeleted(projectId: number) {
-    this.projects = this.projects.filter((project) => project.id !== projectId);
-    this.saveProjectsToLocalStorage();
-    console.log(`Projet avec l'ID ${projectId} supprimé`);
-  }
-
-  onProjectUpdated(updatedProject: Project) {
-    const index = this.projects.findIndex((p) => p.id === updatedProject.id);
-    if (index !== -1) {
-      this.projects[index] = updatedProject;
-      this.saveProjectsToLocalStorage();
-    }
-  }
-
-  private saveProjectsToLocalStorage() {
-    // Protection SSR ici aussi
-    if (isPlatformBrowser(this.platformId)) {
-      localStorage.setItem('projects', JSON.stringify(this.projects));
-    }
-  }
 }
